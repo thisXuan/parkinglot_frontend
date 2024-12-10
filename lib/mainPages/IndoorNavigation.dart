822d023420bf682b54edf7eb95588e8ea8fb7546
thisXuan <1941427986@qq.com>
2024-12-10 18:05:49 +0800

    多层导航

--- lib/mainPages/IndoorNavigation.dart
+++ lib/mainPages/IndoorNavigation.dart
@@ -9,16 +9,24 @@ import 'package:parkinglot_frontend/utils/util.dart';
 class Point {
   final double x;
   final double y;
+  final String floor;
 
-  Point({required this.x, required this.y});
+  Point({
+    required this.x,
+    required this.y,
+    required this.floor
+  });
 
   factory Point.fromJson(Map<String, dynamic> json) {
     return Point(
       x: json['x'].toDouble() as double,
       y: json['y'].toDouble() as double,
+      floor: json['floor'] as String
     );
   }
 }
+
+String _selectedFloor = 'M';
 
 class IndoorNavigationPage extends StatefulWidget {
   @override
@@ -34,17 +42,17 @@ class IndoorNavigationPageState extends State<IndoorNavigationPage> with SingleT
   final TextEditingController idController2 = TextEditingController();
   List<Point> points = [];
   bool _isLoading = false;
+  Map<String, ui.Image?> floorBackgrounds = {};
 
   ui.Image? backgroundImage;
   double _scale = 1.0;
 
   late AnimationController _animationController;
-  String _selectedFloor = 'F3';
 
   @override
   void initState() {
     super.initState();
-    _loadBackgroundImage(_selectedFloor);
+    _loadAllBackgroundImages();
     _animationController = AnimationController(
       vsync: this,
       duration: Duration(seconds: 2),
@@ -58,25 +66,25 @@ class IndoorNavigationPageState extends State<IndoorNavigationPage> with SingleT
     });
   }
 
-  Future<void> _loadBackgroundImage(String floor) async {
-    try {
-      final data = await rootBundle.load("assets/floor/${floor}.jpg");
-      final list = data.buffer.asUint8List();
-      final image = await decodeImageFromList(list);
-
-      setState(() {
-        backgroundImage = image;
-      });
-    } catch (e) {
-      print('Error loading image: $e');
-      ElToast.info("${floor}图片加载失败");
+  Future<void> _loadAllBackgroundImages() async {
+    List<String> floors = ['B1', 'M', 'F1', 'F2', 'F3', 'F4', 'F5'];
+    for (String floor in floors) {
+      try {
+        final data = await rootBundle.load("assets/floor/${floor}.jpg");
+        final list = data.buffer.asUint8List();
+        final image = await decodeImageFromList(list);
+        floorBackgrounds[floor] = image;
+      } catch (e) {
+        print('Error loading image for floor $floor: $e');
+        floorBackgrounds[floor] = null;
+      }
     }
   }
 
   Future<void> getCoordinates(String id1, String id2) async {
     // TODO：根据店铺id返回店铺所在点（占屏幕的百分比）
-    int startId = 222;
-    int endId = 308;
+    int startId = 10;
+    int endId = 200;
 
     dynamic data = {'startId':startId,'endId':endId};
     setState(() {
@@ -108,9 +116,10 @@ class IndoorNavigationPageState extends State<IndoorNavigationPage> with SingleT
     if (floor != null) {
       setState(() {
         _selectedFloor = floor;
+        points = [];
       });
-      _loadBackgroundImage(_selectedFloor); // 切换楼层时加载相应的图片
     }
+
   }
 
   @override
@@ -149,7 +158,7 @@ class IndoorNavigationPageState extends State<IndoorNavigationPage> with SingleT
                   child: CustomPaint(
                     size: Size(double.infinity, double.infinity),
                     painter: IndoorMapPainter(
-                      backgroundImage: backgroundImage,
+                      floorBackgrounds: floorBackgrounds,
                       points: points,
                       scale: _scale,
                       progress: _animationController.value,
@@ -212,13 +221,13 @@ class IndoorNavigationPageState extends State<IndoorNavigationPage> with SingleT
 
 
 class IndoorMapPainter extends CustomPainter {
-  List<Point>? points;
-  ui.Image? backgroundImage;
+  final Map<String, ui.Image?> floorBackgrounds; // 每层楼的背景图
+  final List<Point>? points;
   final double scale;
-  final double progress; // 新增参数，用于控制路径绘制进度
+  final double progress;
 
   IndoorMapPainter({
-    this.backgroundImage,
+    required this.floorBackgrounds,
     this.points,
     required this.scale,
     required this.progress,
@@ -226,37 +235,93 @@ class IndoorMapPainter extends CustomPainter {
 
   @override
   void paint(Canvas canvas, Size size) {
-    if (backgroundImage == null) return;
-
     canvas.save();
     canvas.transform(_transformationController.value.storage);
 
-    // 绘制背景图
-    paintImage(
-      canvas: canvas,
-      rect: Rect.fromLTWH(0, 0, size.width, size.height),
-      image: backgroundImage!,
-      fit: BoxFit.fitWidth,
-    );
+    // 如果 points 为 null 或为空，显示指定的默认背景图
+    if (points == null || points!.isEmpty) {
+      final ui.Image? defaultBackground = floorBackgrounds[_selectedFloor];
+      if (defaultBackground != null) {
+        paintImage(
+          canvas: canvas,
+          rect: Rect.fromLTWH(0, 0, size.width, size.height),
+          image: defaultBackground,
+          fit: BoxFit.fitWidth,
+        );
+      }
+      canvas.restore();
+      return;
+    }
+
+    // 当前绘制的背景图和楼层
+    _selectedFloor = points!.first.floor;
+    ui.Image? currentBackground = floorBackgrounds[_selectedFloor];
+
+    if (currentBackground != null) {
+      paintImage(
+        canvas: canvas,
+        rect: Rect.fromLTWH(0, 0, size.width, size.height),
+        image: currentBackground,
+        fit: BoxFit.fitWidth,
+      );
+    }
 
     // 绘制路径
-    if (points != null && points!.isNotEmpty) {
-      final path = Path();
-      path.moveTo(size.width * points![0].x / 100, size.height * points![0].y / 100);
+    Path path = Path();
+    bool hasPathStarted = false; // 标记路径是否已开始
+    int endIndex = (points!.length * progress).clamp(0, points!.length).toInt();
 
-      int endIndex = (points!.length * progress).clamp(0, points!.length).toInt();
+    for (int i = 0; i < endIndex; i++) {
+      double x = size.width * points![i].x / 100;
+      double y = size.height * points![i].y / 100;
 
-      for (int i = 1; i < endIndex; i++) {
-        double x = size.width * points![i].x/ 100;
-        double y = size.height * points![i].y / 100;
-        path.lineTo(x, y);
+      // 检查当前点的楼层是否切换
+      if (i > 0 && points![i].floor != points![i - 1].floor) {
+        // 在楼层切换时，绘制前一段路径
+        if (hasPathStarted) {
+          final pathPaint = Paint()
+            ..color = Colors.red
+            ..strokeWidth = 4.0
+            ..style = PaintingStyle.stroke;
+          canvas.drawPath(path, pathPaint);
+        }
+
+        //TODO：切换楼层
+
+        // 清除路径并切换背景图
+        path = Path();
+        hasPathStarted = false; // 重置标记
+        canvas.restore();
+        canvas.save();
+
+        // 更新楼层和背景图
+        _selectedFloor = points![i].floor;
+        currentBackground = floorBackgrounds[_selectedFloor];
+        if (currentBackground != null) {
+          paintImage(
+            canvas: canvas,
+            rect: Rect.fromLTWH(0, 0, size.width, size.height),
+            image: currentBackground,
+            fit: BoxFit.fitWidth,
+          );
+        }
       }
 
+      // 更新路径
+      if (!hasPathStarted) {
+        path.moveTo(x, y);
+        hasPathStarted = true; // 标记路径已开始
+      } else {
+        path.lineTo(x, y);
+      }
+    }
+
+    // 绘制最后一段路径
+    if (hasPathStarted) {
       final pathPaint = Paint()
         ..color = Colors.red
         ..strokeWidth = 4.0
         ..style = PaintingStyle.stroke;
-
       canvas.drawPath(path, pathPaint);
     }
 
